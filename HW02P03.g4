48b4d08b9161ec1f7da6e5867641bde4796170b6
Kathy Lenth <klenth@westminstercollege.edu>
2020-01-25 11:27:02 -0700

    Initial commit

---
+++ HW02P03.g4
@@ -0,0 +1,6 @@
+lexer grammar HW02P03;
+
+/* Your lexer rules go here. In ANTLR, names of lexer rules are always UPPERCASE. */
+
+/* If you are using IDEA, the lexer code will placed in the "gen" directory when generated.
+   (Right-click on file, "Generate ANTLR Recognizer," or Ctrl+Shift+G) */
---
+++ src/cmpt355/hw02/p03/Test.java
@@ -0,0 +1,105 @@
+/*
+ * Tester program for lexer grammar of HW2, problem 3.
+ */
+
+package cmpt355.hw02.p03;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.Token;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+    // If you change the name of the lexer, update it here so that this code can find it.
+    public static final String RECOGNIZER_CLASS = "cmpt355.hw02.p03.HW02P03";
+
+    private static StringBuilder buffer = new StringBuilder(100);
+    private static Map<Integer, String> tokenNames = new HashMap<>();
+
+    public static final String INPUT_FILE = "input.txt";
+
+    /* Using reflection, grab a list of all public static final int fields declared in the lexer class to get names of
+       token types */
+    static {
+        try {
+            var lexerClass = Class.forName(RECOGNIZER_CLASS);
+            for (var field : lexerClass.getDeclaredFields()) {
+                int modifiers = field.getModifiers();
+                // Assume it's a token type field if it's public, static, final, an int, and name starts with uppercase
+                if (Modifier.isPublic(modifiers)
+                        && Modifier.isStatic(modifiers)
+                        && Modifier.isFinal(modifiers)
+                        && Integer.TYPE.equals(field.getType())
+                        && Character.isUpperCase(field.getName().charAt(0))) {
+                    String name = field.getName();
+                    int value = field.getInt(null);
+                    tokenNames.put(value, name);
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Could not find lexer class!");
+        } catch (IllegalAccessException ex) {
+            System.err.println("Unable to fetch value of static field");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void main(String... args) throws IOException {
+        try {
+            Class<? extends Lexer> lexerClass = (Class<? extends Lexer>)Class.forName(RECOGNIZER_CLASS);
+            Lexer lexer = newRecognizer(lexerClass, CharStreams.fromFileName(INPUT_FILE));
+            lexer.getAllTokens().forEach(Test::printToken);
+        } catch (ClassNotFoundException ex) {
+            System.err.printf("""
+                    Unable to find recognizer class %s. Have you...
+                      • Generated the recognizer? (IDEA: Right-click .g4 file, Generate ANTLR Recognizer)
+                      • Compiled it?\n""", RECOGNIZER_CLASS);
+        } catch (InvocationTargetException ex) {
+            System.err.printf("Unable to instantiate recognizer class %s because the constructor threw an exception:\n",
+                    RECOGNIZER_CLASS);
+            ex.getTargetException().printStackTrace();
+        } catch (ReflectiveOperationException ex) {
+            System.err.println("""
+                    Unable to instantiate the recognizer class. Exception:""");
+            ex.printStackTrace();
+        }
+    }
+
+    private static <R extends Recognizer<?, ?>> R newRecognizer(Class<R> recognizerClass, Object... arguments)
+                throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        var argTypes = new Class<?>[arguments.length];
+        for (int i = 0; i < arguments.length; ++i)
+            argTypes[i] = (arguments[i] == null) ? Object.class : arguments[i].getClass();
+
+        var constructor = recognizerClass.getDeclaredConstructor(argTypes);
+        return (R)constructor.newInstance(arguments);
+    }
+
+    private static void printToken(Token token) {
+        String tokenName = tokenNames.getOrDefault(token.getType(), "" + token.getType());
+        System.out.printf("%20s {%s}\n", tokenName, escape(token.getText()));
+    }
+
+    private synchronized static String escape(String s) {
+        buffer.setLength(0);
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            buffer.append(switch (c) {
+                case '\n' -> "\\n";
+                case '\r' -> "\\r";
+                case '\t' -> "\\t";
+                case '\\' -> "\\\\";
+                default -> "" + c;
+            });
+        }
+
+        return buffer.toString();
+    }
+}
